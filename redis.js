53155586a2fe56ec3c215752ab18304a9b93963c
root <root@srv1579454.hstgr.cloud>
2026-04-12 10:33:57 +0000

    Lazy-load Redis client to fix env vars timing

--- redis.js
+++ redis.js
@@ -1,8 +1,21 @@
 const { Redis } = require('@upstash/redis');
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+// Lazily create client to ensure env vars are loaded
+let _redis = null;
 
-module.exports = redis;
+function getClient() {
+  if (!_redis) {
+    console.log('UPSTASH_REDIS_REST_URL:', process.env.UPSTASH_REDIS_REST_URL ? 'SET' : 'NOT SET');
+    console.log('UPSTASH_REDIS_REST_TOKEN:', process.env.UPSTASH_REDIS_REST_TOKEN ? 'SET' : 'NOT SET');
+    _redis = new Redis({
+      url: process.env.UPSTASH_REDIS_REST_URL,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    });
+  }
+  return _redis;
+}
+
+module.exports = {
+  get: (...args) => getClient().get(...args),
+  set: (...args) => getClient().set(...args),
+};
